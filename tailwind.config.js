9bbe62ff181c1ab33c97a7ebe849321ebc426883
Jason Yapri <jason.yapri@gmail.com>
2021-09-27 10:41:06 +0700

    Setup Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: 'Proxima Nova',
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        'green': '#1DB954',
+        'dark': '#121212',
+        'light': '#282828',
+        'lightest': '#B3B3B3',
+        'darkest': '#191414',
+      },
+    },
   },
   variants: {
     extend: {},
